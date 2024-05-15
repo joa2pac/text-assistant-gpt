7fa997f9f2526d2e4e92d419cc2825fd43b81d6f
joaquin <joaquinahuel93@gmail.com>
2024-05-14 22:36:41 -0300

    add prosConsDiscusserStream use case and component page

--- src/core/use-cases/index.ts
+++ src/core/use-cases/index.ts
@@ -1,2 +1,3 @@
 export * from "./orthography.use-case";
 export * from "./prosConsDiscusser.use-case";
+export * from "./prosConsStream.use-case";
--- src/presentation/components/pages/pros-cons-stream/ProsConsStreamPage.tsx
+++ src/presentation/components/pages/pros-cons-stream/ProsConsStreamPage.tsx
@@ -1,3 +1,54 @@
+import { useState } from "react";
+import { GptMessages, MyMessages, TypingLoader, TextMessageBox } from "../../../components";
+import { prosConsStreamUseCase } from "../../../../core/use-cases/prosConsStream.use-case";
+
+interface Messages {
+  text: string;
+  isGPT: boolean;
+}
+
 export const ProsConsStreamPage = () => {
-  return <div>ProsConsStreamPage</div>;
+  const [messages, setMessages] = useState<Messages[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handlePost = async (text: string) => {
+    setIsLoading(true);
+
+    setMessages((prev) => [...prev, { text: text, isGPT: false }]);
+
+    await prosConsStreamUseCase(text);
+
+    setIsLoading(false);
+  };
+
+  return (
+    <div className="chat-container">
+      <div className="chat-messages">
+        <div className="grid grid-cols-12 gap-y-2">
+          <GptMessages text="¿Que deseas comparar hoy?" />
+          {messages.map((message, index) =>
+            message.isGPT ? (
+              <GptMessages
+                key={index}
+                text="Hola Mundo, esto es una aplicacion de react, usando la API de apenai xD"
+              />
+            ) : (
+              <MyMessages key={index} text={message.text} />
+            )
+          )}
+
+          {isLoading && (
+            <div className="col-start-1 col-end-12 fade-in">
+              <TypingLoader className="fade-in" />
+            </div>
+          )}
+        </div>
+      </div>
+      <TextMessageBox
+        onSendMessages={handlePost}
+        placeHolder="Typing to message"
+        disabledCorrections
+      />
+    </div>
+  );
 };
